ae422d0cbbda0ee4a9ce016af802cf44ac4efb87
Wendell Pereira <wendelljpereira@gmail.com>
2023-10-31 15:58:54 -0400

    Update QC_and_doublets_identification.R

--- QC_and_doublets_identification.R
+++ QC_and_doublets_identification.R
@@ -1,18 +1,15 @@
 set.seed(1407)
 
-system("mkdir -p logs")
 system("mkdir -p rds_files")
 
-outfile <- "logs/QC_and_doublets_identification.out" # File name of output log
-#Check its existence
+outfile <- "logs/QC_and_doublets_identification.out"
 if ( file.exists(outfile) ) {
-    #Delete file if it exists
     file.remove(outfile)
 }
 
 my_log <- file(outfile) 
-#sink(my_log, append = TRUE, type = "output")
-#sink(my_log, append = TRUE, type = "message")
+sink(my_log, append = TRUE, type = "output")
+sink(my_log, append = TRUE, type = "message")
 
 suppressMessages(require(scDblFinder))
 suppressMessages(require(monocle3))
@@ -24,35 +21,34 @@ suppressMessages( require(dplyr) )
 umi_theshold <- as.numeric(400)
 
 ## A17 WT
-cds_0h <- monocle3::load_cellranger_data("data/A17_sep_2022_0h_10k/",
+cds_0h <- monocle3::load_cellranger_data("data/filtered_A17_0h/",
                                          umi_cutoff = umi_theshold)
-cds_24h <- monocle3::load_cellranger_data("data/A17_sep_2022_24h_10k/",
+cds_24h <- monocle3::load_cellranger_data("data/filtered_A17_24h/",
                                           umi_cutoff = umi_theshold)
-cds_48h <- monocle3::load_cellranger_data("data/A17_sep_2022_48h_10k/",
+cds_48h <- monocle3::load_cellranger_data("data/filtered_A17_48h/",
                                           umi_cutoff = umi_theshold)
-cds_96h <- monocle3::load_cellranger_data("data/A17_sep_2022_96h_10k/",
+cds_96h <- monocle3::load_cellranger_data("data/filtered_A17_96h/",
                                           umi_cutoff = umi_theshold)
 ## sunn-4
-cds_sunn_0h <- monocle3::load_cellranger_data("data/Sunn_sep_2022_0h_10k/",
+cds_sunn_0h <- monocle3::load_cellranger_data("data/filtered_Sunn_0h/",
                                               umi_cutoff = umi_theshold)
-cds_sunn_24h <- monocle3::load_cellranger_data("data/Sunn_sep_2022_24h_10k/",
+cds_sunn_24h <- monocle3::load_cellranger_data("data/filtered_Sunn_24h/",
                                                umi_cutoff = umi_theshold)
-cds_sunn_48h <- monocle3::load_cellranger_data("data/Sunn_sep_2022_48h_10k/",
+cds_sunn_48h <- monocle3::load_cellranger_data("data/filtered_Sunn_48h/",
                                                umi_cutoff = umi_theshold)
-cds_sunn_96h <- monocle3::load_cellranger_data("data/Sunn_sep_2022_96h_10k/",
+cds_sunn_96h <- monocle3::load_cellranger_data("data/filtered_Sunn_96h/",
                                                umi_cutoff = umi_theshold)
-
 ########################
 ## Find doublet cells ##
 ########################
-sample_list <- list("cds_A17_0h" = cds_0h,
-                    "cds_A17_24h" = cds_24h,
-                    "cds_A17_48h" = cds_48h,
-                    "cds_A17_96h" = cds_96h,
-                    "cds_sunn_0h" = cds_sunn_0h,
-                    "cds_sunn_24h" = cds_sunn_24h,
-                    "cds_sunn_48h" = cds_sunn_48h,
-                    "cds_sunn_96h" = cds_sunn_96h)
+sample_list <- list("A17_0h" = cds_0h,
+                    "A17_24h" = cds_24h,
+                    "A17_48h" = cds_48h,
+                    "A17_96h" = cds_96h,
+                    "sunn_0h" = cds_sunn_0h,
+                    "sunn_24h" = cds_sunn_24h,
+                    "sunn_48h" = cds_sunn_48h,
+                    "sunn_96h" = cds_sunn_96h)
 
 for( s in 1:length(sample_list) ) {
     
